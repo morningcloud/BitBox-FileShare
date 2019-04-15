90fd77f0ddb6064e8556bc4e99312801d042b689
Ghawady Ehmaid <ghawadi@yahoo.com>
2019-04-15 21:01:29 +1000

    Added InvalidCommandException.java

---
+++ src/main/java/unimelb/bitbox/Err/InvalidCommandException.java
@@ -0,0 +1,14 @@
+package unimelb.bitbox.Err;
+
+
+public class InvalidCommandException extends Exception {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -9069033831280913568L;
+
+	public InvalidCommandException(String message) {
+        super(message);
+    }
+}
